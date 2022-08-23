1843a8ce15b5669277399e710858ba7849a8b7e2
Vinicius-AMelo <vinicius.apol.melo@gmail.com>
2022-08-23 14:01:58 -0300

    :sparkles: feat: Componente Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Footer.scss'
+
+function Footer() {
+  return (
+    <footer className="main_footer">
+      <div>
+        <ul>
+          <li>CONTATO</li>
+          <li>FRETES E ENTREGA</li>
+          <li>COMO COMPRAR</li>
+          <li>TROCAS E DEVOLUÇÕES</li>
+          <li>SOBRE</li>
+          <li>NA MÍDIA</li>
+          <li>SEGURANÇA</li>
+          <li>POLÍTICAS</li>
+        </ul>
+        <span>
+          <p>CADASTRE-SE E RECEBA NOSSAS NOVIDADES</p>
+          <button type="button">CADASTRAR</button>
+        </span>
+      </div>
+      <hr />
+      <footer>
+        <p>COPYRIGHT @ 2022</p>
+        <ul>
+          <li>FAQ</li>
+          <li>MINHA CONTA</li>
+          <li>MEUS PEDIDOS</li>
+        </ul>
+        <ul>
+          <li>f</li>
+          <li>tw</li>
+          <li>i</li>
+          <li>tu</li>
+          <li>p</li>
+        </ul>
+      </footer>
+    </footer>
+  )
+}
+
+export default Footer
